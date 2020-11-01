d0d17bcf1ac94e6c69dd95e083001235fd19bbaf
Arnold Chand <creativenull@outlook.com>
2020-11-01 15:13:12 -0400

    fix: generate doc comments, equiv to ts types

--- index.d.ts
+++ index.d.ts
@@ -14,6 +14,12 @@ export type Suit = {
 export type SuitColor = 'black' | 'red'
 export type SuitUtf = '\u2660' | '\u2665' | '\u2666' | '\u2663'
 
+export type CardText = {
+  [key: string]: string;
+}
+
+export type SuitText = CardText
+
 declare class Deck {
   static get CARDS (): string[]
   static get SUITS (): Suit[]
@@ -21,18 +27,66 @@ declare class Deck {
   static get SUITS_UTF (): SuitUtf[]
   static get BLANK_CARD_UTF (): string
 
+  /**
+   * Generate a new deck
+   *
+   * @param {boolean} preShuffle Shuffle the deck on initialization
+   */
   constructor (preShuffle: boolean = true): void
 
+  /**
+   * Sort the cards
+   *
+   * @param {Card[]} cards
+   *
+   * @return {Card[]}
+   */
   sort (cards: Card[]): Card[]
 
+  /**
+   * Get a set amount of cards from the deck and remove them from the deck
+   *
+   * @param {number} amount
+   *
+   * @returns {Card[]}
+   */
   getCards (amount: number): Card[]
 
+  /**
+   * Stringify an array of card objects in the format: {id}_{card}{suit}
+   * Eg. ['23#AS', '4#3D', ...]
+   *
+   * @param {Card[]} cards
+   *
+   * @returns {string[]}
+   */
   static stringify (cards: Card[]): string[]
 
+  /**
+   * Parse an array of serialized cards
+   *
+   * @param {string[]} cards
+   *
+   * @returns {Card[]}
+   */
   static parse (cards: string[]): Card[]
 
+  /**
+   * Return the word equivalent of the card suit
+   *
+   * @param {Card} playingCard
+   *
+   * @returns {string}
+   */
   static getSuitText (card: Card): string
 
+  /**
+   * Return the word equivalent of the card value
+   *
+   * @param {Card} playingCard
+   *
+   * @returns {string}
+   */
   static getCardText (card: Card): string
 }
 
